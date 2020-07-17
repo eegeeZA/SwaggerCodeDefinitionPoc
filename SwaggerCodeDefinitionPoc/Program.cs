033dad4985475633e657def5c08acdc2a243fad7
Enrique <git.eegee@0wnd.org>
2020-07-17 13:25:24 +0200

    Generate source code if not present and on rebuild

--- SwaggerCodeDefinitionPoc/Program.cs
+++ SwaggerCodeDefinitionPoc/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Example;
 
 namespace SwaggerCodeDefinitionPoc
 {
@@ -6,7 +7,8 @@ namespace SwaggerCodeDefinitionPoc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var pet = new Pet { Name = "Teddy" };
+            Console.WriteLine("Hello " + pet.ToJson());
         }
     }
 }
